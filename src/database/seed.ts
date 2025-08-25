899cfc5b87a8f05690ee9b500b0a218e7eccba5f
João Gallo <joaogallohd@gmail.com>
2025-08-25 18:34:31 -0300

    implementation of hash on user password

--- src/database/seed.ts
+++ src/database/seed.ts
@@ -1,41 +1,33 @@
 import { db } from "./client.ts";
 import { courses, enrollments, users } from "./schema.ts";
 import { fakerPT_BR as faker } from "@faker-js/faker";
+import { hash } from 'argon2'
 
 async function seed() {
+ 
+  const passwordHash = await hash('123456')
+
   const usersInsert = await db
     .insert(users)
     .values([
       { 
         name: faker.person.fullName(), 
         email: faker.internet.email(),
-        password: '',
+        password: passwordHash,
         role: 'student' 
       },
       { 
         name: faker.person.fullName(), 
         email: faker.internet.email(),
-        password: '',
+        password: passwordHash,
         role: 'student'  
       },
       { 
         name: faker.person.fullName(), 
         email: faker.internet.email(),
-        password: '',
+        password: passwordHash,
         role: 'student'  
-      },
-      { 
-        name: faker.person.fullName(), 
-        email: faker.internet.email(),
-        password: '',
-        role: 'student'  
-      },
-      { 
-        name: faker.person.fullName(), 
-        email: faker.internet.email(),
-        password: '',
-        role: 'student'  
-      },
+      }
     ])
     .returning();
 
