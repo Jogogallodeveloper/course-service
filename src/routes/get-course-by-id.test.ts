7bc7a4fed93ea4c601630f4b7f14761f747d4e5e
João Gallo <joaogallohd@gmail.com>
2025-09-01 19:38:19 -0300

    implementation of jwt on test routine

--- src/routes/get-course-by-id.test.ts
+++ src/routes/get-course-by-id.test.ts
@@ -4,15 +4,18 @@ import { server } from '../app.ts'
 import { faker } from '@faker-js/faker'
 import { courses } from '../database/schema.ts'
 import { makeCourse } from '../tests/factories/make-course.ts'
+import { makeAuthenticatedUser, makeUser } from '../tests/factories/make-user.ts'
 
 
 test('Get course by id', async () => {
     await server.ready()
 
+    const { token } = await makeAuthenticatedUser('student')
     const course = await makeCourse()
 
     const response = await request(server.server)
     .get(`/courses/${course.id}`)
+    .set('Authorization', token)
 
     expect(response.status).toEqual(200)
     expect(response.body).toEqual({
