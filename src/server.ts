1da15107d2145828320e00ba525327837dc70a12
João Gallo <joaogallohd@gmail.com>
2025-09-02 18:59:14 -0300

    fix: prod logger, healthcheck and PORT handling

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { server } from "./app.ts"
 
-server.listen({ port: 3333, host: '0.0.0.0' }).then(() => {
-  console.log('HTTP server running!')
+const port = Number(process.env.PORT) || 3333
+server.listen({ host: '0.0.0.0', port }).then(() => {
+  console.log(`HTTP server running on ${port}`)
 })
